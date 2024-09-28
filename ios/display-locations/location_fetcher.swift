065c7317f0a196762fb7579dd3832ccd586b040f
Nathan Perry <nathanperry@NathanPerry-14mbpr24-m3.local>
2024-09-28 12:29:46 -0600

    feat: fetch a json string

--- ios/display-locations/location_fetcher.swift
+++ ios/display-locations/location_fetcher.swift
@@ -5,3 +5,15 @@
 //  Created by Nathan Perry on 9/28/24.
 //
 
+import Foundation
+
+struct LocationFetcher {
+    var session = URLSession.shared
+
+    func fetchLocations() async throws -> String? {
+        var url = URL.init(string: "https://raw.githubusercontent.com/ncperry/coding-exercises/refs/heads/master/mobile/map-locations/locations.json")!
+        let (data, _) = try await session.data(from: url)
+        let json = String(bytes: data, encoding: String.Encoding.utf8)
+        return json
+    }
+}
---
+++ ios/display-locationsTests/location_fetcherTests.swift
@@ -0,0 +1,19 @@
+//
+//  location_fetcherTests.swift
+//  display-locationsTests
+//
+//  Created by Nathan Perry on 9/28/24.
+//
+
+import Testing
+@testable import display_locations
+
+struct location_fetcherTests {
+
+    @Test func testFetchLocations() async throws {
+        let fetcher = LocationFetcher.init()
+        let json = try await fetcher.fetchLocations()
+        #expect(json!.contains("Golden Gate Grill"))
+    }
+
+}
