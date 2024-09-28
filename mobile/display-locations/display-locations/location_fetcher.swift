4082bf3b182bfe0db9f0188fd9d6677f39f22c9d
Nathan Perry <nathanperry@NathanPerry-14mbpr24-m3.local>
2024-09-28 11:42:25 -0600

    feat: create mobile project

---
+++ mobile/display-locations/display-locations/location_fetcher.swift
@@ -0,0 +1,7 @@
+//
+//  location_fetcher.swift
+//  display-locations
+//
+//  Created by Nathan Perry on 9/28/24.
+//
+
---
+++ mobile/display-locations/display-locationsTests/display_locationsTests.swift
@@ -0,0 +1,16 @@
+//
+//  display_locationsTests.swift
+//  display-locationsTests
+//
+//  Created by Nathan Perry on 9/28/24.
+//
+
+import Testing
+
+struct display_locationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
