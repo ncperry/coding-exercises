c790c9d7f0ebcbf189b967367325c38d707e671f
Nathan Perry <nathanperry@NathanPerry-14mbpr24-m3.local>
2024-09-29 11:19:36 -0600

    feat: display san francisco on initial map load

---
+++ ios/display-locationsTests/map_viewTests.swift
@@ -0,0 +1,23 @@
+//
+//  map_viewTests.swift
+//  display-locationsTests
+//
+//  Created by Nathan Perry on 9/29/24.
+//
+
+import Testing
+import MapKit
+@testable import display_locations
+
+struct map_viewTests {
+
+    @Test func testInitialLocation() async throws {
+        let contentView = await ContentView.init()
+        let sanFranciscoCoordinates = CLLocationCoordinate2D(latitude: 37.7749, longitude: 122.4194)
+        let latitude = contentView.initialMapRegion.region!.center.latitude
+        let longitude = contentView.initialMapRegion.region!.center.longitude
+        #expect(latitude == sanFranciscoCoordinates.latitude)
+        #expect(longitude == sanFranciscoCoordinates.longitude)
+    }
+
+}
