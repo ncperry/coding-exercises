907ebdaadfb70bba2bc7282518da0e1b44c740fb
Nathan Perry <nathanperry@NathanPerry-14mbpr24-m3.local>
2024-09-29 13:09:25 -0600

    spec: remove spec that was making url request

--- ios/display-locationsTests/location_fetcherTests.swift
+++ ios/display-locationsTests/location_fetcherTests.swift
@@ -14,23 +14,18 @@ struct location_fetcherTests {
         #expect(fetcher.url != nil)
     }
 
-    @Test func testFetchLocations() async throws {
-        let fetcher = LocationFetcher.shared
-        let locations = try await fetcher.fetchLocations()
-        #expect(locations[0].id == 1)
-        #expect(locations.count == 45)
-    }
-
     @Test func testSingleton() {
         let fetcher1 = LocationFetcher.shared
         let fetcher2 = LocationFetcher.shared
         #expect(fetcher1 === fetcher2)
     }
 
-    @Test func testConfigureJSONResponse() async throws {
+    @Test func testFetchLocations() async throws {
         let fetcher = LocationFetcher.shared
         fetcher.configureResponse(data: LocationFixtures.sampleResponseData())
         let locations = try await fetcher.fetchLocations()
+        #expect(locations[0].id == 1)
+        #expect(locations[9].id == 10)
         #expect(locations.count == 10)
     }
 }
