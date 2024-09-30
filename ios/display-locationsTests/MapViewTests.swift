d1256f5b2da97b41d62219c9ef425ab84e33d008
Nathan Perry <nathanperry@NathanPerry-14mbpr24-m3.local>
2024-09-29 22:49:36 -0600

    address: swiftui warning

--- ios/display-locationsTests/MapViewTests.swift
+++ ios/display-locationsTests/MapViewTests.swift
@@ -11,11 +11,8 @@ import SwiftUI
 @testable import display_locations
 
 struct MapViewTests {
-    @State private var selection: Int?
-    @State private var presentDetail: Bool = false
-
     @Test func testInitialLocation() async throws {
-        let mapView = await MapView.init(filters: Filters(), selection: $selection, presentDetail: $presentDetail)
+        let mapView = await MapView.init(filters: Filters(), selection: Binding<Int?>.constant(nil), presentDetail: Binding<Bool>.constant(false))
         let sanFranciscoCoordinates = CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194)
         let latitude = await mapView.initialMapRegion.region!.center.latitude
         let longitude = await mapView.initialMapRegion.region!.center.longitude
