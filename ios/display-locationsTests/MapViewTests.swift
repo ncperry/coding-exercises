905f2d9e5f0ebf88bd683e6b42a78f3eac8eb9e3
Nathan Perry <nathanperry@NathanPerry-14mbpr24-m3.local>
2024-09-29 22:44:08 -0600

    feat: fix tests

--- ios/display-locationsTests/MapViewTests.swift
+++ ios/display-locationsTests/MapViewTests.swift
@@ -7,14 +7,18 @@
 
 import Testing
 import MapKit
+import SwiftUI
 @testable import display_locations
 
 struct map_viewTests {
+    @State private var selection: Int?
+    @State private var presentDetail: Bool = false
+
     @Test func testInitialLocation() async throws {
-        let contentView = await ContentView.init()
+        let mapView = await MapView.init(filters: Filters(), selection: $selection, presentDetail: $presentDetail)
         let sanFranciscoCoordinates = CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194)
-        let latitude = await contentView.initialMapRegion.region!.center.latitude
-        let longitude = await contentView.initialMapRegion.region!.center.longitude
+        let latitude = await mapView.initialMapRegion.region!.center.latitude
+        let longitude = await mapView.initialMapRegion.region!.center.longitude
         #expect(latitude == sanFranciscoCoordinates.latitude)
         #expect(longitude == sanFranciscoCoordinates.longitude)
     }
